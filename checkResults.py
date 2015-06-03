ff8de14e4485c73398b032fe121e83bedfa24369
Yu Wang <ywang.chris@gmail.com>
2015-06-03 15:08:43 -0400

    Solution test file

---
+++ checkResults.py
@@ -0,0 +1,20 @@
+from solutions import *
+
+# check the results for problem 1
+problem1list1 = [1,5,9,19,28]
+problem1list2 = ['1','5','9']
+# sumFromList(problem1list2)
+
+
+# check the results for problem 2
+fulllist = interweaveList(problem1list1,problem1list2)
+# print(fulllist)
+
+# check the results for problem 3
+# fibonacciSum(10)
+
+# check the results for problem 4
+largestCombo(problem1list1)
+
+# check the results for problem 5
+find100()
